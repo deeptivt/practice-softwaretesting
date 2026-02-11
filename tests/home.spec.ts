53a7c78b7a8a609f83278516719655b0779de00f
Deeptivt <deeptivt@gmail.com>
2026-02-12 01:39:24 +0530

    added APi Testcases

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,16 +1,25 @@
 import { test, expect } from "@playwright/test";
+
 test.describe("Home page with no auth", () => {
+
   test.beforeEach(async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/");
   });
+
+  test("visual test", async ({ page }) => {
+    await page.waitForLoadState("networkidle");
+    await expect(page).toHaveScreenshot("customer-auth.png", {
+      mask: [page.getByTitle("Practice Software Testing - Toolshop")],
+    });
+  });
+
   test("check sign in", async ({ page }) => {
-    //ensure sign in page
     await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
   });
 
   test("check title of page", async ({ page }) => {
     await expect(page).toHaveTitle(
-      "Practice Software Testing - Toolshop - v5.0",
+      "Practice Software Testing - Toolshop - v5.0"
     );
   });
 
@@ -25,15 +34,27 @@ test.describe("Home page with no auth", () => {
     await page.locator('[data-test="search-submit"]').click();
     await expect(page.getByAltText("Thor Hammer")).toBeVisible();
   });
+
 });
+
 test.describe("home page customer 01 auth", () => {
+
   test.use({ storageState: ".auth/customer01.json" });
+
   test.beforeEach(async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/");
+  });
+
+  test("visualization test authorized", async ({ page }) => {
+   // await page.waitForLoadState("networkidle");
+    await expect(page).toHaveScreenshot("home-page-customer-auth.png", {
+      mask: [page.getByTitle("Practice Software Testing - Toolshop")],
+    });
   });
 
   test("check customer01 is signed in", async ({ page }) => {
     await expect(page.getByTestId("nav-sign-in")).not.toBeVisible();
     await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
   });
+
 });
